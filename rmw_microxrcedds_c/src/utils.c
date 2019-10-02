ef539a89f4a21d00e56533a26a91d0572cd5d1c0
Pablo Garrido <pablogs9@gmail.com>
2019-10-02 11:30:24 +0200

    Type names using "srv" instead of "msg" in services

--- rmw_microxrcedds_c/src/utils.c
+++ rmw_microxrcedds_c/src/utils.c
@@ -236,9 +236,15 @@ int build_topic_xml(
 
   int ret = 0;
   static char type_name_buffer[RMW_TOPIC_NAME_MAX_NAME_LENGTH];
-
+  static char * type_name_union = "msg";
+  
+  if (0 == strncmp(topic_name, "rr", 2) || 0 == strncmp(topic_name, "rq", 2))
+  {
+    type_name_union = "srv";
+  }
+  
   if (RMW_TOPIC_NAME_MAX_NAME_LENGTH >= strlen(topic_name) &&
-    generate_type_name(members, "msg", type_name_buffer, sizeof(type_name_buffer)))
+    generate_type_name(members, type_name_union, type_name_buffer, sizeof(type_name_buffer)))
   {
     char full_topic_name[RMW_TOPIC_NAME_MAX_NAME_LENGTH + 1 + sizeof(ros_topic_prefix)];
 
@@ -270,8 +276,13 @@ int build_xml(
 {
   int ret = 0;
   static char type_name_buffer[RMW_TOPIC_NAME_MAX_NAME_LENGTH];
-
-  if (generate_type_name(members, "msg", type_name_buffer, sizeof(type_name_buffer))) {
+  static char * type_name_union = "msg";
+  
+  if (0 == strncmp(topic_name, "rr", 2) || 0 == strncmp(topic_name, "rq", 2))
+  {
+    type_name_union = "srv";
+  }
+  if (generate_type_name(members, type_name_union, type_name_buffer, sizeof(type_name_buffer))) {
     char full_topic_name[RMW_TOPIC_NAME_MAX_NAME_LENGTH + 1 + sizeof(ros_topic_prefix)];
     full_topic_name[0] = '\0';
 
